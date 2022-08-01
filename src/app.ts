55415c7c465728048434a7216c8f5d2b7e81436f
LordPrinz <plazinski2902@gmail.com>
2022-08-01 12:39:26 +0200

    configure input fields

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class ProjectInput {
 	templateElement: HTMLTemplateElement;
 	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
 
 	constructor() {
 		this.templateElement = <HTMLTemplateElement>(
@@ -11,7 +14,28 @@ class ProjectInput {
 
 		const importedNode = document.importNode(this.templateElement.content, true);
 		this.element = <HTMLFormElement>importedNode.firstElementChild;
+		this.element.id = "user-input";
+
+		this.titleInputElement = <HTMLInputElement>(
+			this.element.querySelector("#title")!
+		);
+		this.descriptionInputElement = <HTMLInputElement>(
+			this.element.querySelector("#description")!
+		);
+		this.peopleInputElement = <HTMLInputElement>(
+			this.element.querySelector("#people")!
+		);
+
+		this.configure();
 		this.attach();
+	}
+
+	private submitHandler(event: Event) {
+		event.preventDefault();
+	}
+
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler.bind(this));
 	}
 
 	private attach() {
