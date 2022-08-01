b6ac915a86f0ddb51d501cab8326031a181be3d1
LordPrinz <plazinski2902@gmail.com>
2022-08-01 23:02:05 +0200

    Improving validation

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,54 @@
+// validation
+
+interface Validatable {
+	value: string | number;
+	required?: boolean;
+	minLength?: number;
+	maxLength?: number;
+	min?: number;
+	max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+	let isValid = true;
+
+	if (validatableInput.required) {
+		isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+	}
+
+	if (
+		validatableInput.minLength != null &&
+		typeof validatableInput.value === "string"
+	) {
+		isValid =
+			isValid && validatableInput.value.length >= validatableInput.minLength;
+	}
+
+	if (
+		validatableInput.maxLength != null &&
+		typeof validatableInput.value === "string"
+	) {
+		isValid =
+			isValid && validatableInput.value.length <= validatableInput.maxLength;
+	}
+
+	if (
+		validatableInput.min != null &&
+		typeof validatableInput.value === "number"
+	) {
+		isValid = isValid && validatableInput.value >= validatableInput.min;
+	}
+
+	if (
+		validatableInput.max != null &&
+		typeof validatableInput.value === "number"
+	) {
+		isValid = isValid && validatableInput.value <= validatableInput.max;
+	}
+
+	return isValid;
+}
+
 // autobind decorator
 
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
@@ -51,10 +102,28 @@ class ProjectInput {
 		const enteredDescription = this.descriptionInputElement.value;
 		const enteredPeople = this.peopleInputElement.value;
 
+		const titleValidatable: Validatable = {
+			value: enteredTitle,
+			required: true,
+		};
+
+		const descriptionValidatable: Validatable = {
+			value: enteredDescription,
+			required: true,
+			minLength: 5,
+		};
+
+		const peopleValidatable: Validatable = {
+			value: +enteredPeople,
+			required: true,
+			min: 1,
+			max: 10,
+		};
+
 		if (
-			enteredTitle.trim().length === 0 ||
-			enteredDescription.trim().length === 0 ||
-			enteredPeople.trim().length === 0
+			!validate(titleValidatable) ||
+			!validate(descriptionValidatable) ||
+			!validate(peopleValidatable)
 		) {
 			throw new Error("Invalid input, please try again!");
 		}
