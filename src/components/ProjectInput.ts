851596ad75d4c14f530f74d9806fe17595acf4d9
LordPrinz <plazinski2902@gmail.com>
2022-08-02 14:55:05 +0200

    Refactore files

---
+++ src/components/ProjectInput.ts
@@ -0,0 +1,149 @@
+// Validation
+
+import autobind from "../decorators/autobind.js";
+
+interface Validatable {
+	value: string | number;
+	required?: boolean;
+	minLength?: number;
+	maxLength?: number;
+	min?: number;
+	max?: number;
+}
+
+function validate(validatableInput: Validatable) {
+	let isValid = true;
+
+	if (validatableInput.required) {
+		isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+	}
+
+	if (
+		validatableInput.minLength != null &&
+		typeof validatableInput.value === "string"
+	) {
+		isValid =
+			isValid && validatableInput.value.length >= validatableInput.minLength;
+	}
+
+	if (
+		validatableInput.maxLength != null &&
+		typeof validatableInput.value === "string"
+	) {
+		isValid =
+			isValid && validatableInput.value.length <= validatableInput.maxLength;
+	}
+
+	if (
+		validatableInput.min != null &&
+		typeof validatableInput.value === "number"
+	) {
+		isValid = isValid && validatableInput.value >= validatableInput.min;
+	}
+
+	if (
+		validatableInput.max != null &&
+		typeof validatableInput.value === "number"
+	) {
+		isValid = isValid && validatableInput.value <= validatableInput.max;
+	}
+
+	return isValid;
+}
+
+// ProjectInput
+
+class ProjectInput {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLFormElement;
+	titleInputElement: HTMLInputElement;
+	descriptionInputElement: HTMLInputElement;
+	peopleInputElement: HTMLInputElement;
+
+	constructor() {
+		this.templateElement = <HTMLTemplateElement>(
+			document.querySelector("#project-input")!
+		);
+		this.hostElement = <HTMLDivElement>document.querySelector("#app")!;
+
+		const importedNode = document.importNode(this.templateElement.content, true);
+		this.element = <HTMLFormElement>importedNode.firstElementChild;
+		this.element.id = "user-input";
+
+		this.titleInputElement = <HTMLInputElement>(
+			this.element.querySelector("#title")!
+		);
+		this.descriptionInputElement = <HTMLInputElement>(
+			this.element.querySelector("#description")!
+		);
+		this.peopleInputElement = <HTMLInputElement>(
+			this.element.querySelector("#people")!
+		);
+
+		this.configure();
+		this.attach();
+	}
+
+	private gatherUserInput(): [string, string, number] | never {
+		const enteredTitle = this.titleInputElement.value;
+		const enteredDescription = this.descriptionInputElement.value;
+		const enteredPeople = this.peopleInputElement.value;
+
+		const titleValidatable: Validatable = {
+			value: enteredTitle,
+			required: true,
+		};
+
+		const descriptionValidatable: Validatable = {
+			value: enteredDescription,
+			required: true,
+			minLength: 5,
+		};
+
+		const peopleValidatable: Validatable = {
+			value: +enteredPeople,
+			required: true,
+			min: 1,
+			max: 10,
+		};
+
+		if (
+			!validate(titleValidatable) ||
+			!validate(descriptionValidatable) ||
+			!validate(peopleValidatable)
+		) {
+			throw new Error("Invalid input, please try again!");
+		}
+
+		return [enteredTitle, enteredDescription, +enteredPeople];
+	}
+
+	private clearInputs() {
+		this.titleInputElement.value = "";
+		this.descriptionInputElement.value = "";
+		this.peopleInputElement.value = "";
+	}
+
+	@autobind
+	private submitHandler(event: Event) {
+		event.preventDefault();
+		const userInput = this.gatherUserInput();
+		if (!Array.isArray(userInput)) {
+			return;
+		}
+		const [title, description, people] = userInput;
+		console.log(title, description, people);
+		this.clearInputs();
+	}
+
+	private configure() {
+		this.element.addEventListener("submit", this.submitHandler);
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement("afterbegin", this.element);
+	}
+}
+
+export default ProjectInput;
---
+++ src/decorators/autobind.ts
@@ -0,0 +1,13 @@
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
+	const adjDescriptor: PropertyDescriptor = {
+		configurable: true,
+		get() {
+			const boundFn = originalMethod.bind(this);
+			return boundFn;
+		},
+	};
+	return adjDescriptor;
+}
+
+export default autobind;
