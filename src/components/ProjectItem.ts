1ec169c038825cdf59168246e47d9b1925af6d62
LordPrinz <plazinski2902@gmail.com>
2022-08-14 22:47:04 +0200

    Add Dragging start feature

--- src/components/ProjectItem.ts
+++ src/components/ProjectItem.ts
@@ -1,8 +1,21 @@
+import autobind from "../decorators/autobind.js";
+import { Draggable } from "../types/Drag.js";
 import Component from "./Component.js";
 import Project from "./Project.js";
 
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem
+	extends Component<HTMLUListElement, HTMLLIElement>
+	implements Draggable
+{
 	private project: Project;
+
+	get persons() {
+		if (this.project.people === 1) {
+			return "1 person";
+		} else {
+			return `${this.project.people} persons`;
+		}
+	}
 
 	constructor(hostId: string, project: Project) {
 		super("single-project", hostId, false, project.id);
@@ -12,12 +25,23 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 		this.renderContent();
 	}
 
-	configure() {}
+	@autobind
+	dragStartHandler(event: DragEvent) {
+		console.log(event);
+	}
+
+	dragEndHandler(_: DragEvent) {
+		console.log("DragEnd");
+	}
+
+	configure() {
+		this.element.addEventListener("dragstart", this.dragStartHandler);
+		this.element.addEventListener("dragend", this.dragEndHandler);
+	}
 
 	renderContent() {
-		console.log(this.project.title);
 		this.element.querySelector("h2")!.textContent = this.project.title;
-		this.element.querySelector("h3")!.textContent = this.project.people.toString();
+		this.element.querySelector("h3")!.textContent = this.persons + " assigned";
 		this.element.querySelector("p")!.textContent = this.project.description;
 	}
 }
